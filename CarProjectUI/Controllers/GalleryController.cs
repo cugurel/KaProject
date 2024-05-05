fc2b96fbbbd7612c6a03ebe86a76458d90e51d06
Cugurel <cugurel7@gmail.com>
2024-05-05 15:29:30 +0300

    gallery controller

--- CarProjectUI/Controllers/GalleryController.cs
+++ CarProjectUI/Controllers/GalleryController.cs
@@ -1,4 +1,5 @@
 ﻿using DataAccess.Concrete.EfRepository;
+using Entity.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CarProjectUI.Controllers
@@ -10,6 +11,33 @@ namespace CarProjectUI.Controllers
         {
             var galleryList = efGalleryRepository.GetAll();
             return View(galleryList);
+        }
+
+        [HttpGet]
+        public IActionResult AddGallery()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddGallery(Gallery gallery)
+        {
+            efGalleryRepository.Add(gallery);
+            return RedirectToAction("Index","Gallery");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateGallery(int id)
+        {
+            var gallery = efGalleryRepository.GetById(id);
+            return View(gallery);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateGallery(Gallery gallery)
+        {
+            efGalleryRepository.Add(gallery);
+            return RedirectToAction("Index", "Gallery");
         }
     }
 }
