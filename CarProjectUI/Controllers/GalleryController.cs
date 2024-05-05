693fa2af2f07a6fe6127cec659c2c0c3cbad12cb
Cugurel <cugurel7@gmail.com>
2024-05-05 14:13:02 +0300

    listing gallery

---
+++ CarProjectUI/Controllers/GalleryController.cs
@@ -0,0 +1,15 @@
+﻿using DataAccess.Concrete.EfRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarProjectUI.Controllers
+{
+    public class GalleryController : Controller
+    {
+        EfGalleryRepository efGalleryRepository = new EfGalleryRepository();
+        public IActionResult Index()
+        {
+            var galleryList = efGalleryRepository.GetAll();
+            return View(galleryList);
+        }
+    }
+}
