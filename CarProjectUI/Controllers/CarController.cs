84a9018d31d53fed53bc0a95ee08892cf8d3376e
Cugurel <cugurel7@gmail.com>
2024-05-11 13:30:12 +0300

    Add car form

---
+++ CarProjectUI/Controllers/CarController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarProjectUI.Controllers
+{
+    public class CarController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult AddCar()
+        {
+            return View();
+        }
+    }
+}
