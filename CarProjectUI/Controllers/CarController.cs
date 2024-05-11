ddf615cf077900a2f0c4c9e6d353cf5baf6b0fa8
Cugurel <cugurel7@gmail.com>
2024-05-11 14:39:52 +0300

    car controller add-list

--- CarProjectUI/Controllers/CarController.cs
+++ CarProjectUI/Controllers/CarController.cs
@@ -1,18 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccess.Concrete.EfRepository;
+using Entity.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CarProjectUI.Controllers
 {
     public class CarController : Controller
     {
+        EfCarRepository carRepository = new EfCarRepository();
         public IActionResult Index()
         {
-            return View();
+            var values = carRepository.GetAll();
+            return View(values);
         }
 
         [HttpGet]
         public IActionResult AddCar()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCar(Car car)
+        {
+            carRepository.Add(car);
+            return Redirect("Index");
         }
     }
 }
--- Entity/Concrete/Car.cs
+++ Entity/Concrete/Car.cs
@@ -15,12 +15,13 @@ namespace Entity.Concrete
         public string Announcement { get; set; }
         public DateTime AnnouncementDate { get; set; }
         public string Brand { get; set; }
-        public string Serial { get; set; }
+        public string? Serial { get; set; }
         public string Model { get; set; }
-        public string Year { get; set; }
-        public string Fuel { get; set; }
-        public string Gear { get; set; }
-        public string Km { get; set; }
+        public string? Year { get; set; }
+        public string? Fuel { get; set; }
+        public string? Gear { get; set; }
+        public string? Km { get; set; }
+        public string? Price { get; set; }
         public int CategoryId { get; set; }
     }
 }
