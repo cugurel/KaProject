42bb8a95e2a3bd9dfa9f47c064f343eda28dff99
Uğur Temel <uqur.temel@gmail.com>
2024-07-11 14:49:40 +0300

    11072024

---
+++ CarProjectUI/Migrations/20240711100336_ugur.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CarProjectUI.Migrations
+{
+    /// <inheritdoc />
+    public partial class ugur : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- CarProjectUI/Program.cs
+++ CarProjectUI/Program.cs
@@ -2,6 +2,36 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+<<<<<<< Updated upstream
+=======
+builder.Services.AddMvc(
+    config =>
+    {
+        config.Filters.Add(new ResponseCacheFilter());
+    }
+);
+builder.Services.AddDbContext<ApplicationContext>(options =>
+options.UseSqlServer("server = (localdb)\\local; database=uqrtml; integrated security = true; TrustServerCertificate=True;", builder => builder.EnableRetryOnFailure()));
+builder.Services.AddIdentity<User,IdentityRole>().
+    AddEntityFrameworkStores<ApplicationContext>().AddDefaultTokenProviders();
+
+
+builder.Services.Configure<IdentityOptions>(options =>
+{ 
+    options.Password.RequireDigit = false;
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 6;
+    options.User.RequireUniqueEmail = true;
+});
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory()).
+    ConfigureContainer<ContainerBuilder>(builder =>
+    {
+        builder.RegisterModule(new AutofacBusinessModule());
+    });
+>>>>>>> Stashed changes
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -21,6 +51,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
+    //pattern: "{controller=Home}/{action=/Auth/Register}");
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.Run();
